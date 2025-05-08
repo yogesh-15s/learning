2d8e48978a39a9e84e0f49b9670af6f50de9dd54
Yogeshwar Shinde <179361680+yogesh-15s@users.noreply.github.com>
2025-05-08 17:09:43 +0000

    learned about variable and data type.

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+#("hello world, this is yogesh")
+#("I am learning python from  30 April 2025")
 
-print("hello world, this is yogesh")
+# DAY 1 
+
+import pandas
+import hashlib
+#above 2 modules not nessasory now
+
+print("hi")
+print(3)
+print(234*876)
+
+a=1234
+print (a)
+b="yogi"
+print(b)
+c= True
+print(c)
+d= None
+print(d)
+print("the type of a is", type(a))
+print("the type of b is", type(b))
+print("the type of c,  is", type(c))
+print("the type of d is", type(d))
