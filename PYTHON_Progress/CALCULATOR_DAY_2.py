0d786094e96c202bc148236f4550d271b6d9e012
Yogeshwar Shinde <179361680+yogesh-15s@users.noreply.github.com>
2025-05-09 16:29:23 +0000

    day2 end

--- PYTHON_Progress/CALCULATOR_DAY_2.py
+++ PYTHON_Progress/CALCULATOR_DAY_2.py
@@ -2,11 +2,22 @@
 #code for calculator.
 
 
-a=432
+#a=432
 
-b=950
+#b=950
 
-print("the value of ",a,"+",b,"is :",a+b)
-print("the value of ",a,"-",b,"is :",a-b)
-print("the value of ",a,"*",b,"is :",a*b)
-print("the value of ",a,"/",b,"is :",a/b)
+#print("the value of ",a,"-",b,"is :",a-b)
+#print("the value of ",a,"*",b,"is :",a*b)
+#print("the value of ",a,"/",b,"is :",a/b)
+
+
+#typecasting
+   #explicit typecasting
+#a="1"
+#b="2"
+#print(int(a)+int(b))
+
+   #implicit typecasting
+#a=1
+#b=2.3
+#print(a+b)
