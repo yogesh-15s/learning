62bcb58c424eb736a0909089dc921ad36c9a9297
Yogeshwar Shinde <179361680+yogesh-15s@users.noreply.github.com>
2025-05-09 14:24:44 +0000

    calculator code done

--- PYTHON_Progress/CALCULATOR_DAY_2.py
+++ PYTHON_Progress/CALCULATOR_DAY_2.py
@@ -1,3 +1,12 @@
 # DAY 2 
 #code for calculator.
 
+
+a=432
+
+b=950
+
+print("the value of ",a,"+",b,"is :",a+b)
+print("the value of ",a,"-",b,"is :",a-b)
+print("the value of ",a,"*",b,"is :",a*b)
+print("the value of ",a,"/",b,"is :",a/b)
